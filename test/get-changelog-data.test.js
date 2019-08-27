5fcee15d1affdb7bcfa1b8161167b188766bd6ec
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2019-08-27 16:19:36 -0700

    Add basic tests to spike existing behavior

--- test/get-changelog-data.test.js
+++ test/get-changelog-data.test.js
@@ -1,5 +1,17 @@
 const test = require('tap').test;
 
+const token = process.env.GITHUB_TOKEN;
+const octokit = require('../src/octokit');
+octokit.authenticate({ type: 'token', token });
+
 const getChangelogData = require('../src/get-changelog-data');
 
-test('getChangelogData');
+test('getChangelogData', async function(t) {
+    const owner = "mapbox";
+    const repo = "mapbox-gl-js";
+    const lastReleaseBase = "dae5fa3c5f30fcaa25158f780972337afdfdfe57"
+    const current = "release-queso"
+
+    const commits = await getChangelogData(octokit, {repo, owner, previous: lastReleaseBase, branch: current});
+    t.equal(commits.length, 53);
+});
--- test/get-latest-release.test.js
+++ test/get-latest-release.test.js
@@ -1,5 +1,16 @@
 const test = require('tap').test;
 
+const token = process.env.GITHUB_TOKEN;
+const octokit = require('../src/octokit');
+octokit.authenticate({ type: 'token', token });
+
 const getLatestRelease = require('../src/get-latest-release');
 
-test('getLatestRelease');
+test('getLatestRelease', async function(t) {
+    const owner = "mapbox";
+    const repo = "mapbox-gl-js";
+
+    const latest = await getLatestRelease(octokit, {repo, owner});
+
+    t.equal(latest, "v1.2.1", "Latest release version");
+});
