6f1955b5223a8981ed3a67fb93b1482ae4356419
Anna Peery <42715836+avpeery@users.noreply.github.com>
2022-10-31 09:36:37 -0700

    list tags failing in remote branch

--- src/get-latest-release.js
+++ src/get-latest-release.js
@@ -2,7 +2,7 @@ const latestSemver = require('latest-semver');
 
 // Find the most recent `vX.Y.Z` release tag
 module.exports = async function getLatestRelease(octokit, {owner, repo}) {
-    let response = await octokit.repos.listTags({owner, repo});
+    let response = await octokit.repos.getTags({owner, repo});
     let {data} = response;
     while (octokit.hasNextPage(response)) {
         response = await octokit.getNextPage(response);
