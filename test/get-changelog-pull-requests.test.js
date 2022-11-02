1f5a39a1fa6535e1a812e2d323ea70814780ae39
Anna Peery <42715836+avpeery@users.noreply.github.com>
2022-11-01 22:24:28 -0700

    fix tests

--- test/get-changelog-pull-requests.test.js
+++ test/get-changelog-pull-requests.test.js
@@ -1,4 +1,4 @@
-const t = require('tap').test;
+const test = require('tap').test;
 
 const token = process.env.GITHUB_TOKEN;
 const octokit = require('../src/octokit');
@@ -6,21 +6,20 @@ octokit.authenticate({ type: 'token', token });
 
 const getChangelogPullRequests = require('../src/get-changelog-pull-requests');
 
-t.test('getChangelogPullRequests finds all relevant pull requests', async function(t) {
+test('getChangelogPullRequests finds all relevant pull requests', async function(t) {
     const owner = 'mapbox';
     const repo = 'mapbox-gl-js';
     const previous = 'v1.1.1';
-    const current = 'v2.10.0';
+    const current = 'v1.2.1';
 
     const pullRequests = await getChangelogPullRequests(octokit, {
         repo, owner, previous, current
     });
 
     t.equal(pullRequests.length, 20, 'Pull request count incorrect');
-    t.end();
 });
 
-t.test('getChangeLogPullRequests filters out PRs cherry picked to the previous release', async function(t) {
+test('getChangeLogPullRequests filters out PRs cherry picked to the previous release', async function(t) {
     const owner = 'mapbox';
     const repo = 'mapbox-gl-js';
     // These versions are tested because v1.2.1 contains a cherry pick.
@@ -33,5 +32,4 @@ t.test('getChangeLogPullRequests filters out PRs cherry picked to the previous r
 
     
     t.equal(pullRequests.length, 41, 'Pull request count incorrect');
-    t.end();
 });
